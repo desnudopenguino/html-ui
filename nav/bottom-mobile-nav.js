0dcad4234527b7b7a1dfc0345d785a77ce4e102b
Bucky Townsend <adam@desnudopenguino@gmail.com>
2022-08-19 01:29:48 -0700

    cleaned up mobile nav. removed nav icon, and simplified js, cleaned up css as well

--- nav/bottom-mobile-nav.js
+++ nav/bottom-mobile-nav.js
@@ -1,20 +1,16 @@
-function toggleZ(element) {
-	document.querySelector('.item-1').style.zIndex = 60;
-	document.querySelector('.item-2').style.zIndex = 50;
-	document.querySelector('.item-3').style.zIndex = 40;
-	document.querySelector('.item-4').style.zIndex = 30;
-	document.querySelector('.item-5').style.zIndex = 20;
-	let navItems = document.querySelectorAll('.item');
-	for (const item of navItems) {
-		item.classList.remove('active');
+document.querySelector('#main-navigation > ul').addEventListener('click', function(e) {
+	if(this.classList.contains('hover')) {
+		document.querySelector('.item-1').style.zIndex = 60;
+		document.querySelector('.item-2').style.zIndex = 50;
+		document.querySelector('.item-3').style.zIndex = 40;
+		document.querySelector('.item-4').style.zIndex = 30;
+		document.querySelector('.item-5').style.zIndex = 20;
+		let navItems = document.querySelectorAll('.item');
+		for (const item of navItems) {
+			item.classList.remove('active');
+		}
+		e.target.style.zIndex = 100;
+		e.target.classList.add('active');
 	}
-	element.style.zIndex = 100;
-	element.classList.add('active');
-	document.querySelector('#main-navigation > ul').classList.remove('hover');
-	document.querySelector('#nav-btn').classList.remove('hidden');
-}
-
-document.getElementById('nav-btn').onclick = function() {
-	this.classList.add('hidden');
-	document.querySelector('#main-navigation > ul').classList.add('hover');
-}
+	this.classList.toggle('hover');
+});
